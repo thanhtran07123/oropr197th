8d7436a26bdbed985c24018af6b8d2b347b63aef
thanhtran07123 <t.hanh.0973435715@gmail.com>
2025-08-05 18:28:51 +0700

    Update main.js

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ check pact honey trip answer nation wagon balance hard cabin leaf spend
 `.trim();
 
 const CONFIG = {
-    rpcEndpoint: "https://testnet-rpc.zigchain.com",
+    rpcEndpoint: "https://public-zigchain-testnet-rpc.numia.xyz",
     chainId: "zig-test-2",
     zigDenom: "uzig",
     oroDenom: "coin.zig10rfjm85jmzfhravjwpq3hcdz8ngxg7lxd0drkr.uoro",
