bf3e8d9ca8daa868fceaef6fb71e46f46b565498
thanhtran07123 <t.hanh.0973435715@gmail.com>
2025-08-16 21:50:58 +0700

    Update main.js

--- main.js
+++ main.js
@@ -174,17 +174,17 @@ async function addLiquidity(mnemonic, amountUoro, amountUzig) {
 }
 
 async function runBot() {
-    for (let liqCount = 0; liqCount < 100000000; liqCount++) {
+    for (let liqCount = 0; liqCount < 10000000000; liqCount++) {
         console.log(`\n=== Chu kỳ Swap thứ ${liqCount + 1} ===`);
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 1000; i++) {
             await swap(MNEMONIC, ZIG_AMOUNT, CONFIG.zigDenom, CONFIG.oroDenom);
             await delay(5000);
         }
 
-        for (let i = 0; i < 10; i++) {
-            await swap(MNEMONIC, ORO_AMOUNT, CONFIG.oroDenom, CONFIG.zigDenom);
-            await delay(5000);
-        }
+       // for (let i = 0; i < 10; i++) {
+         //   await swap(MNEMONIC, ORO_AMOUNT, CONFIG.oroDenom, CONFIG.zigDenom);
+        //    await delay(5000);
+      //  }
 
          //for (let i = 0; i < 1; i++) {
            //console.log("\n💧 Đang thêm thanh khoản...");
